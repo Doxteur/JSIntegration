a2652c63439443a8f5876216999490075462cc74
JimmyD <jdoussain@linaia.com>
2022-11-14 15:14:30 +0100

    Mise en place de CucumberJs

---
+++ test/features/step_definitions/step_defs.js
@@ -0,0 +1,22 @@
+const {Before, Given, When, Then} = require('@cucumber/cucumber');
+
+let x;
+let y;
+
+Given('I Buy a drilling tool worth ${int}', function (int) {
+    x = int;
+    }
+);
+
+Given('I buy the plant worth ${int}', function (int) {
+    y = int;
+    }
+);
+
+Then('I should have ${int} in my account', function (int) {
+    console.log(x+y);
+    }
+);
+
+
+
